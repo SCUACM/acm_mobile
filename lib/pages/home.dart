5c6f870e0575485cd191f832d795651a4064e875
RosalieWessels <rosalie.wessels@yahoo.com>
2024-05-15 10:06:56 -0700

    Started working on club event card

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,8 +16,60 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< Updated upstream
       appBar: AppBar(
         title: const Text("SCU ACM")
+=======
+      appBar: AppBar(title: const Text("SCU ACM")),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.05,
+                    top: MediaQuery.of(context).size.height * 0.02),
+                child: const Text(
+                  "Welcome, Max! 👋",
+                  style: TextStyle(fontSize: 26),
+                  textAlign: TextAlign.left,
+                )),
+          ),
+          Column(
+            children: [
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.topLeft,
+                child: Row(children:[
+                Expanded(
+                  flex: 4, // takes 30% of available width 
+                  child: Column(mainAxisAlignment: MainAxisAlignment.start, 
+                  
+                  children: [
+                    Text("Full Stack Workshop", style: TextStyle(fontSize: 24), maxLines: 4, softWrap: true,),
+                    SizedBox(height: 20),
+                    Flexible(child: Text("Mon Jan 1 - 5:00pm - SCDI 1302", style: TextStyle(fontSize: 20), maxLines: 4, softWrap: true,))
+                      
+                  //ADD TAGS
+                ]),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Align(alignment: Alignment.topCenter, child: Image.asset('assets/images/flyer1.png')),
+                )
+              ]),
+              )
+            ],
+          ),
+          //Expanded(child: ScrollableEventWidget()),
+          // Use ScrollableEventWidget here
+        ],
+>>>>>>> Stashed changes
       ),
       body: Center(
         child: Text("Welcome to the SCU ACM app!")
